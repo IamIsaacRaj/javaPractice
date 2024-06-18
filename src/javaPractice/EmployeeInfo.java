b68cdd119c60dd3be6a58d600d1ac1062686d904
Raju <isaacraj890@gmail.com>
2024-06-18 16:41:40 +0530

    Day 2: Control statements and loops and Praciced Exercises

--- src/javaPractice/EmployeeInfo.java
+++ src/javaPractice/EmployeeInfo.java
@@ -1,7 +1,8 @@
 package javaPractice;
 
 public class EmployeeInfo {
-    public static void main(String[] args) {
+        
+public static void main(String[] args) {
         System.out.println("Primitive Data Types/ variables in Java:");
 
 /*
---
+++ src/javaPractice/Exercises.java
@@ -0,0 +1,18 @@
+package javaPractice;
+
+public class Exercises {
+    public static void main(String[] args) {
+
+/*  Exercise 4:
+    write the code to convert decimal numbers to binary format?
+*/
+        int num = 128;
+        StringBuilder binary = new StringBuilder();
+        while (num != 0){
+            int remainder = num % 2;
+            binary.insert(0, remainder);
+            num = num / 2;
+        }
+        System.out.println("binary value :" + binary);
+    }
+}
