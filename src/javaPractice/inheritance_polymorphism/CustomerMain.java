601fb71f72b5a293c942662cb6818a0425a5a3f1
Raju <isaacraj890@gmail.com>
2024-06-24 18:27:46 +0530

    Day 10: Learned about inheritance in Java and core OOP principles such as method overriding and polymorphism. completed exercise questions

---
+++ src/javaPractice/inheritance_polymorphism/CustomerMain.java
@@ -0,0 +1,21 @@
+package javaPractice.inheritance_polymorphism;
+
+public class CustomerMain {
+    public static void main(String[] args) {
+        // Creating an array of Customer references
+        Customer[] customers = new Customer[3];
+
+        // Adding Customer and PremiumCustomer objects
+        customers[0] = new Customer(101, "Alice", (byte) 28, "alice@example.com", "+91-9876543210", 50000);
+        customers[1] = new PremiumCustomer(202, "Bob", (byte) 45, "bob@example.com", "+91-8765432109", 75000, "Gold");
+        customers[2] = new PremiumCustomer(203, "Carol", (byte) 37, "carol@example.com", "+91-8765432100", 65000, "Silver");
+
+        // Displaying customer details and discount percentages using polymorphism
+        for (Customer customer : customers) {
+            customer.displayCustomerDetails();
+            byte discount = customer.discountPercentageByAge();
+            System.out.println("Discount Percentage: " + discount + "%");
+            System.out.println("-----------------------------");
+        }
+    }
+}
---
+++ src/javaPractice/inheritance_polymorphism/exercise2/Manager.java
@@ -0,0 +1,30 @@
+package javaPractice.inheritance_polymorphism.exercise2;
+
+public class Manager extends Employee{
+
+    double bonusPercentage;
+
+    public Manager(){
+        super();
+    }
+
+    public Manager(int employeeId, String employeeName, char gender, byte age, double salary, double bonusPercentage) {
+        super(employeeId, employeeName, gender, age, salary);
+        this.bonusPercentage = bonusPercentage;
+    }
+
+    @Override
+    public double calculateBonus() {
+        return salary * bonusPercentage/100  ;
+    }
+    @Override
+    void displayEmployeeInfo() {
+        super.displayEmployeeInfo();
+        System.out.println("bonusPercentage = " + bonusPercentage);
+        System.out.println("calculated bonus = " + calculateBonus());
+        System.out.println("Salary with bonus = " + calculateTotalSalary());
+    }
+
+
+
+}
